1aac5cb05acfd4e434c0264b5bdf69a39dc8bde7
MCUmbrella <mcumbrella@qq.com>
2020-05-14 17:23:34 +0800

    Update MMPROTECT.java

--- src/main/java/vip/floatationdevice/MMPROTECT.java
+++ src/main/java/vip/floatationdevice/MMPROTECT.java
@@ -6,6 +6,7 @@ import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
 import org.spongepowered.api.event.entity.DamageEntityEvent;
+import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.filter.cause.Root;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
@@ -17,16 +18,16 @@ import java.util.logging.Logger;
 @Plugin(id = "mmprotect", name="MMPROTECT", version="1.0-SNAPSHOT",description = "MiniMine Protect")
 public class MMPROTECT {
     @Inject
-    Logger logger;
+    private Logger logger;
 
     @Listener
     public void svrStart(GameStartedServerEvent event) {
-        logger.info("MMPROTECT started.");
+        System.out.println("MMPROTECT started.");
     }
 
     @Listener
     public void svrStop(GameStoppingServerEvent event) {
-        logger.info("MMPROTECT stopping.");
+        System.out.println("MMPROTECT stopping.");
     }
 
     @Listener
@@ -36,16 +37,16 @@ public class MMPROTECT {
             //check if the original state
             if (tr.getOriginal().getState().getType() == BlockTypes.BEDROCK) {
                 //if not, set the transaction invalid
-                logger.warning("Detected & cancelled bedrock breaking caused by " + player.getName());
+                //System.out.println("Detected & cancelled bedrock breaking caused by " + player.getName());
                 tr.setValid(false);
             }
         }
     }
-
-    public void onDamage(DamageEntityEvent evt, @Root Player player) {
-        if (evt.getBaseDamage() > 45) {
-            evt.setBaseDamage(45);
-            logger.warning("Detected base damage >45 caused by " + player.getName() + ". Setting base damage to 45");
+    @Listener
+    public void onDamageEntityEvent(DamageEntityEvent evt) {
+        if (evt.getBaseDamage() > 40) {
+            evt.setBaseDamage(40);
+            //System.out.println("Detected base damage >40");
         }
     }
 
